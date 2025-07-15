b07220728aae3d372c9b5bab47c912525be9f88d
Vitaly Safonov <vitaliysafonov@livedune.com>
2025-07-15 16:51:14 +0300

    Оптимизация компонента App: улучшена работа с состоянием, добавлена синхронизация с Redux, исправлены типы и убраны дублирования. Обновлен метод fetchTodos для обработки ошибок и улучшения читаемости кода.

--- src/store/action-creators/todos.ts
+++ src/store/action-creators/todos.ts
@@ -2,35 +2,41 @@ import axios from "axios";
 import { ApiTask, TodoAction, TodoActionTypes } from "../../types";
 import { Dispatch } from "redux";
 
-
-export const fetchTodos = (page= 1, limit= 10) => {
-    return async (dispatch: Dispatch<TodoAction>): Promise<ApiTask[]> => {
-        {
-    try {
-        dispatch({type: TodoActionTypes.FETCH_TODOS})
-      const response = await axios.get<ApiTask[]>(
-        "https://jsonplaceholder.typicode.com/todos", {
-          params: {_page: page, limit: limit}
+export const fetchTodos = (page = 1, limit = 10) => {
+  return async (dispatch: Dispatch<TodoAction>): Promise<ApiTask[]> => {
+    {
+      try {
+        dispatch({ type: TodoActionTypes.FETCH_TODOS });
+        const response = await axios.get<ApiTask[]>(
+          "https://jsonplaceholder.typicode.com/todos",
+          {
+            params: { _page: page, limit: limit },
+          }
+        );
+        const data: ApiTask[] = response.data;
+        setTimeout(() => {
+          dispatch({
+            type: TodoActionTypes.FETCH_TODOS_SUCCESS,
+            payload: response.data,
+          });
+        }, 1000);
+        return data;
+      } catch (error) {
+        dispatch({
+          type: TodoActionTypes.FETCH_TODOS_ERROR,
+          payload: "Произошла ошибка при загрузке задач",
+        });
+        if (error instanceof Error) {
+          console.error("Axios error:", error.message);
+        } else {
+          console.error("Unexpected error:", error);
         }
-      );
-      const data: ApiTask[] = response.data;
-      setTimeout(()=> {
-      dispatch({type: TodoActionTypes.FETCH_TODOS_SUCCESS, payload: response.data})
-      }, 1000)
-      return data;
-    } catch (error) {
-        dispatch({type: TodoActionTypes.FETCH_TODOS_ERROR, payload: "Произошла ошибка при загрузке задач"})
-      if (error instanceof Error) {
-        console.error("Axios error:", error.message);
-      } else {
-        console.error("Unexpected error:", error);
+        return [];
       }
-      return [];
     }
-  }
-    }
-}
+  };
+};
 
 export function setTodoPage(page: number): TodoAction {
-  return {type: TodoActionTypes.SET_TODO_PAGE, payload: page}
+  return { type: TodoActionTypes.SET_TODO_PAGE, payload: page };
 }
